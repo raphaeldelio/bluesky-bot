904cd1831e084ac3c2b3764008341082a7e3cc1e
Raphael De Lio <raphadlts@gmail.com>
2024-12-01 13:18:52 +0100

    Adding Prefix to DynamicConfigService.kt

--- src/main/kotlin/dev/raphaeldelio/service/DynamicConfigService.kt
+++ src/main/kotlin/dev/raphaeldelio/service/DynamicConfigService.kt
@@ -2,11 +2,15 @@ package dev.raphaeldelio.service
 
 class DynamicConfigService(private val redisService: RedisService) {
 
+    companion object {
+        private const val PREFIX = "config:"
+    }
+
     fun getVersion(): String {
-        return redisService.get("version") ?: "0.1.0"
+        return redisService.get("${PREFIX}version") ?: "0.1.0"
     }
 
     fun setVersion(version: String) {
-        redisService.set("version", version)
+        redisService.set("${PREFIX}version", version)
     }
 }
